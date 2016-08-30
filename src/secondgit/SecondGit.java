b7603e92161377fb568dc846bff0e0dd5f54c5c4
cpalmer10 <cpalmer10@my.wctc.edu>
2016-08-30 16:32:19 -0500

    added animal class and  modified main to include 2nd message

---
+++ src/secondgit/SecondGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secondgit;
+
+/**
+ *
+ * @author Chris
+ */
+public class SecondGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello Class");
+    }
+    
+}
